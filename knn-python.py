fca1cd6d44b814547bd6ec65f05418186a6cd59f
MatthieuLouf <m.louf@orange.fr>
2019-04-14 10:19:54 +0200

    centrer reduire data test

--- knn-python.py
+++ knn-python.py
@@ -26,7 +26,8 @@ def lecture_dataset() :
 
     return data_set
 
-def centrer_reduire(data_set):
+
+def centrer_reduire_data(data_set):
     data_set_centre_reduit= data_set.copy()
     moy = []
     ecart_type = []
@@ -38,18 +39,31 @@ def centrer_reduire(data_set):
     for i in range(len(data_set[0])-1):
         for j in range(len(data_set)):
             data_set_centre_reduit[j][i] = (data_set[j][i] -moy[i])/ecart_type[i]
-            #print(data_set_centre_reduit[j])
-            
-    
-    return data_set_centre_reduit
+            #print(data_set_centre_reduit[j]) 
 
+    return data_set_centre_reduit, moy,ecart_type
+
+
+def centrer_reduire_test(data_test,moyenne,ecart_type):
+    data_test_centre_reduit = data_test.copy()
+    for i in range(len(data_test)):
+        data_test_centre_reduit[i] = (data_test[i] -moyenne[i])/ecart_type[i]
+
+    return data_test_centre_reduit
+
+
+#def calcul_distance(data_set,data_test):
 
 
 data_set = lecture_dataset()
-data_set_centre_reduit = centrer_reduire(data_set)
+data_set_centre_reduit,moyenne,ecart_type = centrer_reduire_data(data_set)
+
+data_test = [6.5,3.4,5,2]
+data_test_centre_reduit = centrer_reduire_test(data_test,moyenne,ecart_type)
+
 
 #print(data_set_centre_reduit)
-
+print(data_test_centre_reduit)
 
 
 #print(mean(data_set_centre_reduit[j][0]for j in range(len(data_set) ) ))
